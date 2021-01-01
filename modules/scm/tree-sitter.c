62a7e85affbed0e4c1ab6ba4d751f1860482a7de
Valeriy Stukanov <vst@localhost.localdomain>
2021-01-02 01:56:59 +0200

    guile tree-sitter node api

--- modules/scm/tree-sitter.c
+++ modules/scm/tree-sitter.c
@@ -1,4 +1,9 @@
+#include "libguile/alist.h"
 #include "libguile/foreign.h"
+#include "libguile/hashtab.h"
+#include "libguile/list.h"
+#include "libguile/modules.h"
+#include "libguile/numbers.h"
 #include <tree_sitter/api.h>
 #include <libguile.h>
 
@@ -32,8 +37,38 @@ api_tree_cursor_node_type(SCM _cursor)
   return scm_from_utf8_string(ts_node_type(current_node));
 }
 
+SCM
+api_tree_cursor_node(SCM _cursor)
+{
+  TSTreeCursor *cursor = (TSTreeCursor *) scm_to_pointer(_cursor);
+  TSNode current_node = ts_tree_cursor_current_node(cursor);
+
+  SCM alist = scm_make_list(scm_from_uint32(0), scm_from_uint32(0));
+
+  TSPoint start_point = ts_node_start_point(current_node);
+  TSPoint end_point = ts_node_end_point(current_node);
+
+  scm_acons(scm_from_utf8_symbol("start-row"),
+	    scm_from_uint32(start_point.row),
+	    alist);
+
+  scm_acons(scm_from_utf8_symbol("start-column"),
+	    scm_from_uint32(start_point.column),
+	    alist);
+
+  scm_acons(scm_from_utf8_symbol("end-row"),
+	    scm_from_uint32(end_point.row),
+	    alist);
+
+  scm_acons(scm_from_utf8_symbol("end-column"),
+	    scm_from_uint32(end_point.column),
+	    alist);
+
+  return alist;
+}
+
 void
-init_tree_sitter (void *unused)
+init_tree_sitter_cursor (void *unused)
 {
   scm_c_define_gsubr("goto-first-child", 1, 0, 0, &api_tree_cursor_goto_first_child);
   scm_c_define_gsubr("goto-next-sibling", 1, 0, 0, &api_tree_cursor_goto_next_sibling);
@@ -50,5 +85,5 @@ init_tree_sitter (void *unused)
 void
 scm_init_tree_sitter_module()
 {
-  scm_c_define_module("bow tree-sitter cursor", &init_tree_sitter, NULL);
+  scm_c_define_module("bow tree-sitter cursor", &init_tree_sitter_cursor, NULL);
 }
--- scripts/init.scm
+++ scripts/init.scm
@@ -1,16 +1,22 @@
 (use-modules ((bow tree-sitter cursor)
 	      #:prefix cursor:))
 
-(define (walk-node cursor parents)
+(define (walk-node cursor context)
   (let* ((node-type (cursor:current-node-type cursor)))
+    (display (make-string (* 2 context) #\space))
+    (display "- ")
     (display node-type)
     (newline)
     (when (cursor:goto-first-child cursor)
-      (walk-node cursor parents)
+      (walk-node cursor (+ 1 context))
+
+      (while (cursor:goto-next-sibling cursor)
+	(walk-node cursor (+ 1 context)))
+      
       (cursor:goto-parent cursor))))
 
 (define-public (parse-tree cursor)
-  (walk-node cursor (list)))
+  (walk-node cursor 0))
 
 (define-public (main)
   (find-file (string-append editor-base "sample/simple.js")))
